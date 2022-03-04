8ad99961ff539350fbe52cc89fcf7c6546bcd2d4
asghaami000 <100955866+asghaami000@users.noreply.github.com>
2022-03-04 10:40:16 -0600

    Create tools.js

---
+++ tools.js
@@ -0,0 +1,54 @@
+let score = 0;
+let game = 0; 
+let question = 0; 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  math = createButton("mathmatics");
+  math.position(windowWidth/2, windowHeight/2);
+  math.mousePressed(maths);
+ 
+
+  english2 = createButton("english");
+  english2.position(windowWidth/2-200, windowHeight/2);
+  english2.mousePressed(englishs);
+  let inp = createInput('');
+  inp.position(windowWidth, windowHeight);
+  inp.size(100);
+  inp.hide();
+
+}
+
+function draw() {
+  if(game == 0){
+    math.show();
+    english2.show();
+  }else{
+    math.hide();
+    english2.hide();
+    inp.show();
+  }
+
+  if(game == 1){
+    
+  }
+
+  if(game == 2){
+    
+  }
+
+
+
+
+  
+}
+
+function maths(){
+  game = 1;
+}
+
+
+function englishs(){
+  game = 2;
+}
